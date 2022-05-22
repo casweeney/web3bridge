20973f625b618df6be4c5843b060efc0e9d941ba
CodingCas <casweeno2000@gmail.com>
2022-05-22 13:17:06 +0100

    UI layout completed

---
+++ src/components/leaderboard/Leader.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Leader = () => {
+  return (
+    <div className="col-md-12">
+        <div className="card">
+            <div className="card-body">
+                <h4 className="card-heading">
+                    Casweeney Ojukwu
+                </h4>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Leader
---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import LeaderList from '../leaderboard/LeaderList';
+
+const Home = () => {
+  return (
+    <div className="container-fluid">
+        <LeaderList />
+    </div>
+  )
+}
+
+export default Home;
